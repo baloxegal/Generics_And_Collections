c4c432e8c844b65a4dac53dafc9e0b17680509ff
baloxegal <baloxegal@gmail.com>
2021-03-10 22:12:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Generics_And_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10];
+            a[0] = default(int);
+            int? b = 4;
+            if(b.HasValue) Console.WriteLine(b.Value);
+           
+            Console.WriteLine(b.Value == 4 ? 2 : 0);
+            b = null;
+            var y = b ?? 10;
+            Console.WriteLine(y);
+        }
+    }
+}
